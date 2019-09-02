6806d40deb19c1ec4690f029bb62492c07f9340b
Jim Hillhouse <jdhouse4@me.com>
2019-09-02 18:18:14 -0500

    Initial Commit

---
+++ BuzzWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuzzWithSwiftUI
+//
+//  Created by James Hillhouse IV on 9/2/19.
+//  Copyright © 2019 PortableFrontier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
