bc20c6a51b9783f9f0fe3d91bf584eb9eb0bfba9
Jim Hillhouse <jdhouse4@me.com>
2019-09-29 23:31:31 -0500

    Almost Done

--- BuzzWithSwiftUI/SceneKitView.swift
+++ BuzzWithSwiftUI/SceneKitView.swift
@@ -30,12 +30,6 @@ struct SceneKitView: UIViewRepresentable {
 
     var changingLightNode: SCNNode = SCNNode()
 
-    var lightIndex: Int = 0 // Directional
-
-    var lightBulbImageNode: SKSpriteNode = SKSpriteNode(imageNamed: "lightbulbHighlighted")
-
-    var lightSwitchImageNode: SKSpriteNode = SKSpriteNode(imageNamed: "lightSwitchHighlighted")
-
     var lightTextNode: SKLabelNode = SKLabelNode(fontNamed: "HelveticaNeue")
 
     var overlayScene: SKScene = SKScene()
@@ -56,11 +50,11 @@ struct SceneKitView: UIViewRepresentable {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
 
-        // place the camera
+        // Place the camera
         cameraNode.position = SCNVector3(x: 0, y: 10, z: 20)
 
+        changingLightNode.name = "ChangingLightNode"
         changingLightNode.light = SCNLight()
-        changingLightNode.light!.name = "ChangingLightNode"
         changingLightNode.light!.type = .directional
         changingLightNode.light!.intensity = 500.0
         changingLightNode.light!.categoryBitMask = 2
@@ -80,46 +74,14 @@ struct SceneKitView: UIViewRepresentable {
         //createOverlayScene(overlayScene: overlayScene, screenSize: screenSize)
         overlayScene.size = CGSize(width: screenSize.width, height: screenSize.height)
 
-        // Add-in SKLabelNode for the title
-        let headerTextNode = SKLabelNode(fontNamed: "HelveticaNeue")
-        headerTextNode.text = "Light Rendering Issues"
-        headerTextNode.fontSize = 24
-        headerTextNode.fontColor = SKColor.white
-        headerTextNode.position = CGPoint(
-            x: screenCenter.x,
-            y: screenSize.height - 75.0)
-        overlayScene.addChild(headerTextNode)
-
-        //Add-in the SKSpriteNode for the button to change the light type
-        lightBulbImageNode.name = "lightBulbImageNode"
-        lightBulbImageNode.xScale = 2.0
-        lightBulbImageNode.yScale = 2.0
-        lightBulbImageNode.position = CGPoint(
-            x: screenCenter.x * 1.5,
-            y: 100)
-        overlayScene.addChild(lightBulbImageNode)
-
-        lightSwitchImageNode.name = "lightSwitchImageNode"
-        lightSwitchImageNode.xScale = 2.0
-        lightSwitchImageNode.yScale = 2.0
-        lightSwitchImageNode.position = CGPoint(
-            x: screenCenter.x * 0.5,
-            y: 100)
-        overlayScene.addChild(lightSwitchImageNode)
-
         // Add-in SKLabelNode for the light currently in use
+        lightTextNode.name = "SunlightTypeTextNode"
         lightTextNode.text = changingLightNode.light!.type.rawValue
         lightTextNode.fontSize = 20
         lightTextNode.fontColor = .white
         lightTextNode.position = CGPoint(x: screenCenter.x,
-                                         y:  lightBulbImageNode.position.y + lightBulbImageNode.frame.size.height / 2.0)
+                                         y:  50)
         overlayScene.addChild(lightTextNode)
-
-        let tappedGesture = UITapGestureRecognizer(target: context.coordinator,
-                                                   action: #selector(Coordinator.buttonTapped(gesture:)))
-
-
-        scnView.addGestureRecognizer(tappedGesture)
 
         scnView.overlaySKScene = overlayScene
 
@@ -141,6 +103,8 @@ struct SceneKitView: UIViewRepresentable {
         scnView.showsStatistics = true
 
         toggleBuzzFaceLamp(scnView)
+
+        toggleSunlight(scnView)
     }
 
 
@@ -150,6 +114,35 @@ struct SceneKitView: UIViewRepresentable {
 
         lightNode!.isHidden = lightSwitch
     }
+
+
+
+    func toggleSunlight(_ scnView: SCNView) {
+        let sunlightNode = scnView.scene!.rootNode.childNode(withName: "ChangingLightNode", recursively: true)
+        //let sunlightTypeTextNode = scnView.scene!.rootNode.childNode(withName: "SunlightTypeTextNode", recursively: true)!
+
+        
+        //print(sunlightTypeTextNode)
+
+        switch sunlightSwitch {
+        case 0:
+            sunlightNode!.light?.type = .directional
+            //sunlightTypeTextNode.text = sunlightNode!.light?.type.rawValue
+        case 1:
+            sunlightNode!.light?.type = .spot
+            //sunlightTypeTextNode.text = sunlightNode!.light?.type.rawValue
+        case 2:
+            sunlightNode!.light?.type = .omni
+            //sunlightTypeTextNode.text = sunlightNode!.light?.type.rawValue
+        case 3:
+            sunlightNode!.light?.type = .ambient
+            //sunlightTypeTextNode.text = sunlightNode!.light?.type.rawValue
+        default:
+            sunlightNode!.light?.type = .directional
+            //sunlightTypeTextNode.text = sunlightNode!.light?.type.rawValue
+        }
+    }
+
 
 
 
@@ -166,77 +159,6 @@ struct SceneKitView: UIViewRepresentable {
             self._lightSwitch = lightSwitch
             self._sunlightSwitch = sunlightSwitch
         }
-
-
-        @objc func buttonTapped(gesture: UITapGestureRecognizer) {
-            print("Button tapped")
-
-            // Retrieve the SKScene.
-            let scnOverlayScene: SKScene = scnView.overlayScene
-
-            // Determine the location within the view the gesture occured.
-            let p = gesture.location(in: gesture.view)
-
-            // Convert the node tapped from  view coords to scene coords.
-            let hitResult = scnOverlayScene.convertPoint(fromView: p)
-
-            // Determine which node was tapped.
-            let hitNode = scnOverlayScene.atPoint(hitResult)
-
-            // But first, set-up a sequence of SKAction events.
-            let lightBulbAction = SKAction.sequence(
-                [SKAction.scaleX(to: 0, duration: 0.05),
-                 SKAction.scale(to: 2, duration: 0.05)
-            ])
-
-
-            // Give the user some indication which sprite was touched.
-            if hitNode.name == "lightBulbImageNode"
-            {
-
-                // Run the SKAction
-                scnView.lightBulbImageNode.run(lightBulbAction)
-
-                // Increment the lightIndex
-                scnView.lightIndex += 1
-
-                if scnView.lightIndex == 4
-                {
-                    scnView.lightIndex = 0
-                }
-
-                switch scnView.lightIndex {
-                case 0:
-                    scnView.changingLightNode.light?.type = .directional
-                    scnView.lightTextNode.text = scnView.changingLightNode.light?.type.rawValue
-                case 1:
-                    scnView.changingLightNode.light?.type = .spot
-                    scnView.lightTextNode.text = scnView.changingLightNode.light?.type.rawValue
-                case 2:
-                    scnView.changingLightNode.light?.type = .omni
-                    scnView.lightTextNode.text = scnView.changingLightNode.light?.type.rawValue
-                case 3:
-                    scnView.changingLightNode.light?.type = .ambient
-                    scnView.lightTextNode.text = scnView.changingLightNode.light?.type.rawValue
-                default:
-                    scnView.changingLightNode.light?.type = .directional
-                    scnView.lightTextNode.text = scnView.changingLightNode.light?.type.rawValue
-                }
-            }
-
-            if hitNode.name == "lightSwitchImageNode"
-            {
-                scnView.lightSwitchImageNode.run(lightBulbAction)
-
-                let lightNode = scnView.scene.rootNode.childNode(withName: "BuzzFaceLight", recursively: true)
-
-                // Toggle Buzz' face lamp
-                scnView.lightSwitch.toggle()
-
-                lightNode!.isHidden = scnView.lightSwitch
-            }
-        }
-
 
 
         @objc func updateBuzzFaceLamp(sender: Any) {
